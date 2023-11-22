f7211dd0e23e32698eafc7f5fa5b687e2ec88ce8
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2023-11-22 12:06:49 +0100

    dépot job09 jour03 modif

--- jour03/job09/moyenne.py
+++ jour03/job09/moyenne.py
@@ -3,25 +3,22 @@ def moyenne(note1,note2,note3):
     
     return ((float(note1)+float(note2)+float(note3))/3)
     
-
-
 note1=input("rentrez la note1 :")
 note2=input("rentrez la note2 :")
 note3=input("rentrez la note3 :")
-moyenne_etudiant = moyenne(note1,note2,note3)
-
 
 """===code==="""
 
-
-moyenne(note1,note2,note3)
+moyenne_etudiant = int(moyenne(note1,note2,note3))
 
 print ("la moyenne etudiante est  : ",moyenne_etudiant)
-if   20<=  moyenne_etudiant >= 15:
+if   20 >=  moyenne_etudiant <= 15:
     print("Très bon élève")
-elif  14 <= moyenne_etudiant >= 11:
+elif  14 >= moyenne_etudiant <= 11:
     print("Bon élève")
-elif  10 <=moyenne_etudiant >= 8:
+elif  10 >=moyenne_etudiant <= 8:
     print("Élève moyen")
-elif  7 <= moyenne_etudiant >= 0:
+elif  7 >= moyenne_etudiant <= 0:
     print("Élève devant faire des efforts")
+else:
+    print("moyenne invalide")
