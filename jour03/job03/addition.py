cceb4f90183b03180ab1d5e3299b5ae8fef47830
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2023-11-22 10:19:58 +0100

    dépot job03 jour03

---
+++ jour03/job03/addition.py
@@ -0,0 +1,8 @@
+def Add(nombre1,nombre2):
+    resultat = 0
+    resultat = nombre1 + nombre2
+    print(nombre1,"+",nombre2,"=",resultat)
+    print("--------------------------------")
+Add(1,3)
+Add(4,5)
+Add(3,3)
