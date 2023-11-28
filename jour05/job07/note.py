42eda57006005edfbbe5db0a7268a1851e22a1b7
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2023-11-28 10:20:47 +0100

    update jours05 job07

--- jour05/job07/note.py
+++ jour05/job07/note.py
@@ -20,24 +20,3 @@ def arrondi(l):
 
 print(l)
 print(arrondi(l))
-
-
-def arrondir_notes(notes):
-    notes_arrondies = []
-    for notes in notes:
-        if notes < 40:
-            notes_arrondies.append(notes)
-        else:
-            reste = notes % 5
-            if reste >= 3:
-                notes += 5 - reste
-            notes_arrondies.append(notes)
-    return notes_arrondies
-            
-
-    
-
-#Déclaration des variables : 
-
-
-print(arrondir_notes(l))
